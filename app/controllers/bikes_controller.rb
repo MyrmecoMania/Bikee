f6b95881ef56760f19edb597283c7f805ff6b77a
Gautier David <gautier.d.91@gmail.com>
2022-11-21 17:05:11 +0100

    bikes controller

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,7 +1,53 @@
 class BikesController < ApplicationController
+  before_action :set_bike, only: %i[show edit destroy update]
+
   def index
+    @bikes = Bike.all
   end
 
   def show
+    @bike = Bike.find(params[:id])
+  end
+
+  def new
+    @bike = Bike.new
+  end
+
+  def create
+    @bike = Bike.new(bike_params)
+    @bike.user = current_user
+    if @bike.save
+      redirect_to bike_path(@bike)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    # @bike = Bike.find(params[:id])
+  end
+
+  def update
+    if @bike.update(bike_params)
+      redirect_to bike_path(@bike)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    # @bike = Bike.find(params[:id])
+    @bike.destroy
+    redirect_to bikes_path, status: :see_other
+  end
+
+  private
+
+  def bike_params
+    params.require(:bikes).permit(:category, :status, :price_per_day, :address, :user_id)
+  end
+
+  def set_bike
+    @bike = Bike.find(params[:id])
   end
 end
