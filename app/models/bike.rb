d4eb096bb71a9b8d20029a6ba50f0f73798c6bbb
Gautier David <gautier.d.91@gmail.com>
2022-11-23 12:25:46 +0100

    cloudinary setup done and seeds with rand images and address for bike

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -2,6 +2,8 @@ class Bike < ApplicationRecord
   belongs_to :user
   has_one :rent
 
+  has_many_attached :photos
+
   validates :category, presence: true
   validates :price_per_day, presence: true, numericality: { only_integer: true }
   validates :address, presence: true
